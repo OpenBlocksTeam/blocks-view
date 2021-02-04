558ae6a078cb07ba60c1230db8f2232f5da5064c
Iyxan23 <nurihsanalghifari@gmail.com>
2021-02-04 08:56:47 +0700

    feat: Add is_bottom

--- lib/src/main/java/com/iyxan23/blocks/view/SketchwareBlock.java
+++ lib/src/main/java/com/iyxan23/blocks/view/SketchwareBlock.java
@@ -11,6 +11,9 @@ public class SketchwareBlock {
     public String id;
     public ArrayList<SketchwareField> parameters;
 
+    // Indicates if this block can't have a next_block, e.g. Finish Activity block
+    public boolean is_bottom;
+
     int next_block;
 
     public int color;
@@ -23,6 +26,10 @@ public class SketchwareBlock {
         this.parameters = parameters;
         this.color = color;
         this.color_dark = Utilities.manipulateColor(color, 0.7f);
+
+        // next_block is -1 if there is nothing after it
+        this.is_bottom = next_block == -1;
+    }
 
     public void draw(Canvas canvas, Paint rect_paint, Paint text_paint, int blocks_down) {
         int block_height = 60;
@@ -48,14 +55,15 @@ public class SketchwareBlock {
         rect_paint.setColor(color_dark);
         canvas.drawRect(50, top_position, 400, bottom_position + shadow_height, rect_paint);
         
-        canvas.drawRect(100, top_position, 175, bottom_position + shadow_height + block_outset_height, rect_paint);
+        if (!is_bottom)
+            canvas.drawRect(100, top_position, 175, bottom_position + shadow_height + block_outset_height, rect_paint);
 
         // Draw the actual block
         rect_paint.setColor(color);
         canvas.drawRect(50, top_position, 400, bottom_position, rect_paint);
 
-
-        canvas.drawRect(100, top_position, 175, bottom_position + block_outset_height, rect_paint);
+        if (!is_bottom)
+            canvas.drawRect(100, top_position, 175, bottom_position + block_outset_height, rect_paint);
 
         // Draw the block's text
         // TODO: ADD A FORMATTER

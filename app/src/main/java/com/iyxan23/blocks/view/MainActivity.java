35a43fef58912fba2f8d5a1606054c3730e427a0
Iyxan23 <nurihsanalghifari@gmail.com>
2021-02-04 13:40:07 +0700

    add: Added SketchwareBlocksView to :app

--- app/src/main/java/com/iyxan23/blocks/view/MainActivity.java
+++ app/src/main/java/com/iyxan23/blocks/view/MainActivity.java
@@ -2,21 +2,7 @@ package com.iyxan23.blocks.view;
 
 import android.os.Bundle;
 
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.view.View;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-
-import com.iyxan23.blocks.view.databinding.ActivityMainBinding;
-
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 

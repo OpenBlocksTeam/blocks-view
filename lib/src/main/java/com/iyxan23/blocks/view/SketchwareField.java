805e694a2b13a5dd1228f98f188682482d3ea257
Iyxan23 <nurihsanalghifari@gmail.com>
2021-02-04 15:06:57 +0700

    add: Add SketchwareField.draw

--- lib/src/main/java/com/iyxan23/blocks/view/SketchwareField.java
+++ lib/src/main/java/com/iyxan23/blocks/view/SketchwareField.java
@@ -1,5 +1,8 @@
 package com.iyxan23.blocks.view;
 
+import android.graphics.Canvas;
+import android.graphics.Paint;
+
 import java.util.ArrayList;
 
 public class SketchwareField {
@@ -10,6 +13,9 @@ public class SketchwareField {
 
     public SketchwareBlock block;
 
+    Paint text_paint = new Paint();
+    Paint rect_paint = new Paint();
+
     /**
      * This will initialize this class as a SketchwareBlock (return value block)
      * @param block The block
@@ -17,6 +23,7 @@ public class SketchwareField {
     public SketchwareField(SketchwareBlock block) {
         this.block = block;
         is_block = true;
+        init();
     }
 
     /**
@@ -26,5 +33,38 @@ public class SketchwareField {
     public SketchwareField(String value) {
         this.value = value;
         is_block = false;
+        init();
+    }
+
+    private void init() {
+        text_paint.setColor(0xFF000000);
+        text_paint.setAntiAlias(true);
+        text_paint.setStyle(Paint.Style.FILL);
+        text_paint.setTextSize(18f);
+
+        rect_paint.setStyle(Paint.Style.FILL);
+        rect_paint.setColor(0xFFFFFFFF);
+        rect_paint.setAntiAlias(true);
+    }
+
+    public int getWidth() {
+        // Padding for the text should only be about 5
+        if (!is_block) {
+            return (int) text_paint.measureText(value) + 5 * 2;
+        } else {
+            // TODO
+        }
+    }
+
+    public void draw(Canvas canvas, int left, int top, int bottom) {
+        if (!is_block) {
+            // Draw the white background
+            canvas.drawRect(left, top, left + getWidth(), bottom, rect_paint);
+
+            // Draw the text / value
+            canvas.drawText(value, left + 5, top, text_paint);
+        } else {
+            // TODO
+        }
     }
 }

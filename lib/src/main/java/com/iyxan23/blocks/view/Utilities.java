c3d3556b3ba23fe01adc565e4b4bc09f473bbad4
Iyxan23 <nurihsanalghifari@gmail.com>
2021-02-06 10:19:36 +0700

    add: Add dp-px converters

--- lib/src/main/java/com/iyxan23/blocks/view/Utilities.java
+++ lib/src/main/java/com/iyxan23/blocks/view/Utilities.java
@@ -1,5 +1,6 @@
 package com.iyxan23.blocks.view;
 
+import android.content.Context;
 import android.graphics.Color;
 
 public class Utilities {
@@ -20,5 +21,29 @@ public class Utilities {
                 Math.min(r,255),
                 Math.min(g,255),
                 Math.min(b,255));
+    }
+
+    // http://www.androidtutorialshub.com/android-convert-dp-px-px-dp/
+
+    /**
+     * This method converts device specific pixels to density independent pixels.
+     *
+     * @param px A value in px (pixels) unit. Which we need to convert into db
+     * @param context Context to get resources and device specific display metrics
+     * @return A float value to represent dp equivalent to px value
+     */
+    public float convertPxToDp(Context context, float px) {
+        return px / context.getResources().getDisplayMetrics().density;
+    }
+
+    /**
+     * This method converts dp unit to equivalent pixels, depending on device density.
+     *
+     * @param dp A value in dp (density independent pixels) unit. Which we need to convert into pixels
+     * @param context Context to get resources and device specific display metrics
+     * @return A float value to represent px equivalent to dp depending on device density
+     */
+    public float convertDpToPx(Context context, float dp) {
+        return dp * context.getResources().getDisplayMetrics().density;
     }
 }

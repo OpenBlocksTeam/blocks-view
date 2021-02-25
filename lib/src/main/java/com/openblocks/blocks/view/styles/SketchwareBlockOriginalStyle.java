84c93a11e1c8f688df5d9243833a2f2032b13087
Iyxan23 <nurihsanalghifari@gmail.com>
2021-02-25 13:23:29 +0700

    style: Remove one space

--- lib/src/main/java/com/openblocks/blocks/view/styles/SketchwareBlockOriginalStyle.java
+++ lib/src/main/java/com/openblocks/blocks/view/styles/SketchwareBlockOriginalStyle.java
@@ -19,7 +19,7 @@ public class SketchwareBlockOriginalStyle extends SketchwareBlock {
     public void draw(Context context, Canvas canvas, Paint rect_paint, Paint text_paint, int top, int left, int height, int shadow_height, int block_outset_left_margin, int block_outset_width, int block_outset_height, boolean is_overlapping, int previous_block_color) {
         // super.draw(canvas, rect_paint, text_paint, top, left, height, shadow_height, block_outset_left_margin, block_outset_width, block_outset_height, is_overlapping, previous_block_color);
 
-        NinePatchDrawable sketchwareBlock =  (NinePatchDrawable) ResourcesCompat.getDrawable(context.getResources(), R.drawable.sketchware_block, context.getTheme());
+        NinePatchDrawable sketchwareBlock = (NinePatchDrawable) ResourcesCompat.getDrawable(context.getResources(), R.drawable.sketchware_block, context.getTheme());
         if (sketchwareBlock != null) {
             sketchwareBlock.setBounds(left, top, getWidth(text_paint), getHeight(text_paint));
             sketchwareBlock.draw(canvas);

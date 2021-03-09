05f247020e3b9c4010620e6e5604726b97423626
Iyxan23 <nurihsanalghifari@gmail.com>
2021-03-09 15:01:53 +0700

    add: Migrate to DrawHelper

--- lib/src/main/java/com/openblocks/blocks/view/SketchwareNestedBlock.java
+++ lib/src/main/java/com/openblocks/blocks/view/SketchwareNestedBlock.java
@@ -79,7 +79,21 @@ public class SketchwareNestedBlock extends SketchwareBlock {
     }
 
     @Override
-    public void draw(Context context, Canvas canvas, Paint rect_paint, Paint text_paint, int top, int left, int height, int shadow_height, int block_outset_left_margin, int block_outset_width, int block_outset_height, boolean is_overlapping, int previous_block_color) {
+    public void draw(Context context,
+                     Canvas canvas,
+                     Paint rect_paint,
+                     Paint text_paint,
+                     int top,
+                     int left,
+                     int height,
+                     int shadow_height,
+                     int block_outset_left_margin,
+                     int top_block_outset_left_margin,
+                     int block_outset_width,
+                     int block_outset_height,
+                     boolean is_overlapping,
+                     int previous_block_color
+    ) {
         Paint original_block_paint = new Paint();
         original_block_paint.setColor(rect_paint.getColor());
         original_block_paint.setTextSize(rect_paint.getTextSize());
@@ -89,7 +103,7 @@ public class SketchwareNestedBlock extends SketchwareBlock {
         int block_outset_left = left + block_outset_left_margin;
 
         // Draw the childes! (similar to SketchwareBlocksView)
-        previous_block_color = color;
+        int childes_previous_block_color = color;
         int block_height = getBlockHeight(text_paint);
         int previous_top_position = top + shadow_height;
         int previous_block_height = block_height - shadow_height;  // Because if not, the first block would get overlapped by the event
@@ -125,35 +139,33 @@ public class SketchwareNestedBlock extends SketchwareBlock {
                             block_outset_width,
                             block_outset_height,
                             is_overlapping,
-                            previous_block_color
+                            childes_previous_block_color
                     );
 
-            previous_block_color = current_block.color;
+            childes_previous_block_color = current_block.color;
         }
 
-        Paint extensions_paint = new Paint(rect_paint);
-
         int bottom_block_top_position = top + getHeight(text_paint) + shadow_height - block_bottom_height;
-        int bottom_block_bottom_position = top + getHeight(text_paint);
 
-        // draw the bottom part's shadow
-        extensions_paint.setColor(DrawHelper.manipulateColor(this.color, 0.7f));
-        canvas.drawRect(left, bottom_block_top_position, left + getWidth(text_paint), bottom_block_bottom_position + shadow_height, extensions_paint);
+        DrawHelper.drawRectSimpleOutsideShadow(canvas, left, bottom_block_top_position, getWidth(text_paint), block_bottom_height, shadow_height, color);
 
-        extensions_paint.setColor(this.color);
-        // Then draw the bottom part
-        canvas.drawRect(left, bottom_block_top_position, left + getWidth(text_paint), bottom_block_bottom_position, extensions_paint);
+        if (!is_overlapping) {
+            // Ohk no, draw the outset with shadow and the top block's outset
 
-        // Don't forget the outset
-        if (is_overlapping) {
-            canvas.drawRect(block_outset_left, bottom_block_top_position, block_outset_left + block_outset_width, bottom_block_bottom_position + block_outset_height, extensions_paint);
+            // Draw the outset
+            DrawHelper.drawRectSimpleOutsideShadow(canvas, left + block_outset_left_margin, top + getHeight(text_paint), block_outset_width, block_outset_height + shadow_height, shadow_height, color);
+
+            // Draw the top block's outset
+            DrawHelper.drawRect(canvas, left + top_block_outset_left_margin, top, block_outset_width, block_outset_height, DrawHelper.manipulateColor(previous_block_color, 0.8f));
         } else {
-            canvas.drawRect(block_outset_left, bottom_block_top_position, block_outset_left + block_outset_width, bottom_block_bottom_position + block_outset_height, extensions_paint);
-            canvas.drawRect(block_outset_left, bottom_block_top_position, block_outset_left + block_outset_width, bottom_block_bottom_position + block_outset_height - shadow_height, extensions_paint);
+            // Yes, just draw the top block's outset
+
+            // Draw the top block's outset
+            DrawHelper.drawRect(canvas, left + top_block_outset_left_margin, top, block_outset_width, block_outset_height, previous_block_color);
         }
 
         // Ok, draw the "indent"
-        canvas.drawRect(left, top + block_height, left + indent_width, bottom_block_bottom_position, extensions_paint);
+        DrawHelper.drawRectSimpleOutsideShadow(canvas, left, top, indent_width, getHeight(text_paint) + shadow_height, shadow_height, color);
 
         drawParameters(context,canvas, left, top, top + ((getBlockHeight(text_paint) + shadow_height + block_outset_height + text_padding) / 2), getBlockHeight(text_paint), shadow_height, text_paint);
     }
